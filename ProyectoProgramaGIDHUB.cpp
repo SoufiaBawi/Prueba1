dfbc64b947ea7055f20bb2f618576306745c177a
SoufiaBawi <floresvargassandrasofia@gmail.com>
2023-11-14 23:22:09 -0600

    ModificadoSofia

--- ProyectoProgramaGIDHUB.cpp
+++ ProyectoProgramaGIDHUB.cpp
@@ -1,27 +1,64 @@
 #include <iostream>//librerias
+#include <cmath>
 using namespace std;
 int main(){//funcion principal
     int Variable_opcion=0;//variable parael switch
     int suma_materias=0;
     int V_cuantas_materias=0;//variable para saber cuantas materias son
-    cout << "Menu de opciones";
-    cout << "\n1:promedio de calificacione";//imprime el menu
+    int arreglo_materias[V_cuantas_materias];//inivcialzar el arreglo para almacenar las calificaciones
+    cout << "\n\nMenu de opciones";
+    cout << "\n1. Promedio de calificaciones";//imprime el menu
+    cout << "\n2. Calcular areas de triangulos";
+    cout << "\n Elige una opciÃ³n: ";
     cin >> Variable_opcion;//lee la opcion
     switch(Variable_opcion){//este es el cuerpo de los casos
-             case 1://en caso que elija 1
+            case 1://en caso que elija 1
                   cout << "Ingresa el numero de calificaciones para calcular el promedio: ";//cuantas calificaciones va apromediar
                   cin >> V_cuantas_materias;//leer dato
-                  int arreglo_materias[V_cuantas_materias];//inivcialzar el arreglo para almacenar las calificaciones
                   for (int i1=0; i1<V_cuantas_materias; i1++){//leer y sumar las calificaciones
                       cout << "\nIngeresa la calificacion " << i1 << ": ";
                       cin >> arreglo_materias[i1];
-                      suma_materias=suma_materias+arreglo_materias[i1];
+                      suma_materias+=arreglo_materias[i1];
                   }
-                  float promedio_materias = 0.0;//inicialiar variable para el promedio
+                  float promedio_materias;//inicialiar variable para el promedio
                   promedio_materias = suma_materias / V_cuantas_materias;//sacar el promedio
                   cout << "\nEl promedio de las materias es de: " << promedio_materias;//imprimir el promedio
-             break;
-             
-    }
-    
-}
+            break;
+            case 2:
+                cout<<"\nHa elegido la opcion dos, eliga el triangulo:\n";
+                cout<<"a. Triangulo Equilatero\n";
+                cout<<"b. Cuadrado\n";
+                cout<<"c. Circulo\n'";
+            char opc2;
+                cout<<"Eliga una opcion: ";
+                cin>>opc2;
+            float b, h, area, l, radio;
+            switch(opc2){
+                case 'a':
+                cout<<"\nIngresa la base: ";
+            
+                cin>>b;
+                cout<<"\nIngresa la altura: ";
+                cin>>h;
+
+                area = b * h / 2;
+                cout<<"\nEl area de tu triangulo será: "<<area;
+                    break;
+
+                case 'b':
+                    cout<<"\nIngresa un lado : ";
+                    cin>>l;
+                    area = l*l;
+                    cout<<"\nEl area de tu cuadrado será: "<<area; 
+                    break;
+
+                case 'c':
+                    cout<<"\nIngresa el radio: ";
+                    cin>>radio;
+
+                    area = M_PI * std::pow(radio, 2);
+                    cout << "\nEl area del circulo es: " << area;
+                    break;
+            }
+    	}
+	main();
